2a9a98f7ab37b6f7164e52fbc034bd7a057dc033
hieuemmm <hieus247@gmail.com>
2022-10-29 07:03:55 +0700

    Done Assignment JAVA WEB

--- JWEB_PA101/src/main/resources/CMS.sql
+++ JWEB_PA101/src/main/resources/CMS.sql
@@ -7,8 +7,8 @@ GO
 create table content(
 	[id_content] int IDENTITY(1,1) primary key 
       ,[title] nvarchar(50)
-      ,[brief] nvarchar(250)
-      ,[content] nvarchar(250)
+      ,[brief] nvarchar(150)
+      ,[content] nvarchar(1000)
       ,[created_date] nvarchar(50)
       ,[update_time] nvarchar(50)
       ,[sort] nvarchar(50)
@@ -17,13 +17,13 @@ create table content(
 
 create table member
 ([id_member] int IDENTITY(1,1) primary key 
-      ,[first_name] nvarchar(50)
-      ,[last_name]nvarchar(50)
+      ,[first_name] nvarchar(30)
+      ,[last_name]nvarchar(30)
       ,[user_name]nvarchar(50)
-      ,[password]nvarchar(50)
+      ,[password]nvarchar(30)
       ,[phone]nvarchar(50)
       ,[email]nvarchar(50)
-      ,[description]nvarchar(50)
+      ,[description]nvarchar(200)
       ,[created_date]nvarchar(50)
       ,[update_time]nvarchar(50)
 ,[id_content] int FOREIGN KEY  REFERENCES content ([id_content]))
